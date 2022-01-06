b4fdc300ee5c39892c4b2143529f11bb9bbbb663
dyl-lee <dylanlee1701@gmail.com>
2022-01-06 17:51:15 -0500

    v2 scraping script

--- scraping.py
+++ scraping.py
@@ -1,80 +1,100 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # Import Splinter and BeautifulSoup
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
-
-# set up splinter
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-# visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
-
-# search elements with div tag and attribute list_text (i.e. <div class='list_text'>) with optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
-
-# set up html parser
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+import datetime as dt
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-### Article scraping
+def scrape_all():
+    # initialize browser, create data dictionary, end the web driver and return scraped data
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-# search slide_elem for div class='content_title', find() only searches for the first class/attribute we specify. find_all() retrieves all tags and attributes
-slide_elem.find('div', class_='content_title').get_text()
+    news_title, news_paragraph = mars_news(browser)
+    
+    # run all scraping functions and store results in dictionary
+    data = {
+        'news_title': news_title,
+        'news_paragraph': news_paragraph,
+        'featured_image': featured_image(browser),
+        'facts': mars_facts(),
+        'last_modified': dt.datetime.now()
+    }
+    # stop webdriver and return data
+    browser.quit()
+    return data
 
-# store only title text in new variable
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+### Titles and summaries
+def mars_news(browser):
+    # visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-# same for article_teaser, this will retrieve the first article that satisfies the tag/attribute
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+    # search elements with div tag and attribute list_text (i.e. <div class='list_text'>) with optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    # convert browser html to soup object
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    # add try/except block for error handling (in case webpage format changes and breaks the script)
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
+
+        # Use the parent element to find the first <a> tag and save it as `news_title`
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
+
+    # return to tell python function is complete to use title and paragraph outside the function
+    return news_title, news_p
 
 
 ### Featured Images
+def featured_image(browser):
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Visit URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+    # find and click the full image button (second button on the page, hence index [1])
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# find and click the full image button (second button on the page, hence index [1])
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
-
-# Find the relative image url by (1) find the img tag with class fancybox-image, then .get('src') to pull the link to the image
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
-
-# use base URL to create a complete URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
-img_url
+    # add try/except for error handling
+    try:
+        # Find the relative image url by (1) find the img tag with class fancybox-image, then .get('src') to pull the link to the image
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    except AttributeError:                                      # refers to invalid attribute
+        return None
+    # use base URL to create a complete URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    
+    return img_url
 
 
 ### Mars facts
+def mars_facts():
+    try:
+    # scrape the entire Mars facts table
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]    # pd.read_html() specifically returns a list of tables found in html, 
+                                                                # by specifying [0], pandas pulls only the first table it finds and turns it into a DF
+    except BaseException:                                       # BaseException is a general exception, used to catch multiple error types
+        return None                                             
 
-# scrape the entire Mars facts table
-df = pd.read_html('https://galaxyfacts-mars.com')[0] #pd.read_html() specifically returns a list of tables found in html, 
-#by specifying [0], pandas pulls only the first table it finds and turns it into a DF
+    # assign the column headers and set the index to be the Description column
+    df.columns=['Description', 'Mars','Earth']
+    df.set_index('Description', inplace=True)
 
-# assign the column headers and set the index to be the Description column
-df.columns=['Description', 'Mars','Earth']
-df.set_index('Description', inplace=True)
-df
+    # make df html-ready for web application
+    return df.to_html()
 
-# make df html-ready for web application
-df.to_html()
-
-# tell splinter to quit browser
-browser.quit()
-
+if __name__ == "__main__":
+    # if running as script, print scraped data
+    print(scrape_all())
