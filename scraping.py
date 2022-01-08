7241e515ab95a481801947561917a504326e35c1
dyl-lee <dylanlee1701@gmail.com>
2022-01-08 10:03:24 -0500

    made headless=True

--- scraping.py
+++ scraping.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def scrape_all():
     # initialize browser, create data dictionary, end the web driver and return scraped data
     executable_path = {'executable_path': ChromeDriverManager().install()}
-    browser = Browser('chrome', **executable_path, headless=False)
+    browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
     
